4176ac0b7f0875eebb1a24f1e3a8f1c28f3411d8
Lucas Soares <lucas.soares.araujo@hotmail.com>
2019-07-01 01:11:09 -0300

    feat: Listagem de Provedores de Serviço

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:8000/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,7 +26,7 @@ class User extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatarId' });
+    this.belongsTo(models.File, { foreignKey: 'avatarId', as: 'avatar' });
   }
 
   checkPassword(password) {
