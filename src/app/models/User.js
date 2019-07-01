fa2391568e0899c24fb577546888466f3a339f3d
Lucas Soares <lucas.soares.araujo@hotmail.com>
2019-07-01 00:08:53 -0300

    feat: criação de avatar para usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,10 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatarId' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.passwordHash);
   }
